0c7b8a66ce37abb4621f4aedc01937bd083dc5ab
Jakob <jakob.hust@gmail.com>
2019-10-18 14:30:53 +0200

    Scriptable object now saves the clips

--- MMFusionLWRP/Assets/Scripts/Scriptable Object Scripts/AnimContainer.cs
+++ MMFusionLWRP/Assets/Scripts/Scriptable Object Scripts/AnimContainer.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "Animation Clip Container", menuName = "Anim Clip Container", order = 10)]
+[System.Serializable]
 public class AnimContainer : ScriptableObject
 {
     public AnimationClip[] animationClips;
