0fbe7f8a18d23a55b44a3d8963aa46351780f543
Jannik <jneerd15@student.aau.dk>
2019-10-21 10:45:41 +0200

    Added a bit more to avoid compile errors

--- MMFusionLWRP/Assets/Scripts/GameEvent/BaseGameEventListener.cs
+++ MMFusionLWRP/Assets/Scripts/GameEvent/BaseGameEventListener.cs
@@ -2,7 +2,7 @@
 using UnityEngine.Events;
 
 public abstract class BaseGameEventListener<T, E, UER> : MonoBehaviour, 
-    IGameEventListener<T> where : BaseGameEvent<T> where UER : UnityEvent<T>
+    IGameEventListener<T> where E : BaseGameEvent<T> where UER : UnityEvent<T>
 {
 	[SerializeField] private E gameEvent;
 	public E GameEvent { get { return gameEvent; } set { gameEvent = value; } }
@@ -24,4 +24,12 @@ public abstract class BaseGameEventListener<T, E, UER> : MonoBehaviour,
 
 		GameEvent.UnregisterListener(this);
 	}
+
+    public void OnEventRaised(T item)
+    {
+	    if (unityEventResponse != null)
+	    {
+		    unityEventResponse.Invoke(item);
+	    }
+    }
 }
