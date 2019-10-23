d3fd0879f9dded2bea1b0f673873846fb106661f
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-10-23 10:33:55 +0200

    Fusion added

--- MMFusionLWRP/Assets/Scriptable Objects/Variable Holders/FloatReferenceDrawer.cs
+++ MMFusionLWRP/Assets/Scriptable Objects/Variable Holders/FloatReferenceDrawer.cs
@@ -6,24 +6,25 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(FloatReference))]
 public class FloatReferenceDrawer : PropertyDrawer
 {
-    private bool propertyUI, showMenu = false;
+    // private bool propertyUI, showMenu = false;
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {   
         bool useUnique = property.FindPropertyRelative("UseUnique").boolValue;
 
-        
+        // Choosing the settings icon for the drawer. 
         GUIContent popupIcon = EditorGUIUtility.IconContent("_Popup");
         //propertyUI = EditorGUILayout.BeginFoldoutHeaderGroup(propertyUI, "Property UI");
         EditorGUI.BeginProperty(position, label, property);
 
         position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-        var rect = new Rect(position.position, Vector2.one * 15);
-
+        // Position is the position of the InputField, here i displace x by 15, so the button is shown to the left of it. 
+        var rect = new Rect(new Vector2(position.x -15, position.y), Vector2.one * 15);
+        
+        // Code for the actual button, and the menu inside containing Unique and Variable. 
         if (
             EditorGUI.DropdownButton(rect,
                 popupIcon, 
-                FocusType.Passive , 
+                FocusType.Keyboard , 
                 new GUIStyle() { fixedWidth = 50f, border = new RectOffset(1,1,1,1)}))
         {
             GenericMenu menu = new GenericMenu();
@@ -43,12 +44,13 @@ public class FloatReferenceDrawer : PropertyDrawer
 
         //position.position = Vector2.right;
         float value = property.FindPropertyRelative("UniqueValue").floatValue;
-
+        // If Unique chosen in menu
         if(useUnique) {
             string newValue = EditorGUI.TextField(position, value.ToString());
             float.TryParse(newValue, out value);
             property.FindPropertyRelative("UniqueValue").floatValue = value;
         }
+        // If Variable is chosen
         else {
             EditorGUI.ObjectField(position, property.FindPropertyRelative("Variable"), GUIContent.none);
         }
@@ -61,7 +63,7 @@ public class FloatReferenceDrawer : PropertyDrawer
         else
             useUnique = true; */
     }
-
+    // Function to get the boolean from the FloatReference script, and sets it from this script. 
     private void SetProperty(SerializedProperty property, bool value) {
         var propRelative = property.FindPropertyRelative("UseUnique");
         propRelative.boolValue = value;
